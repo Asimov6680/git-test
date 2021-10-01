9b95dda07de0b484e5849cdef22cf022d62c1e7d
Asimov6680 <79745114+Asimov6680@users.noreply.github.com>
2021-10-01 15:03:03 +0800

    pull commit

--- src/main/java/com/axm/git/GitTest.java
+++ src/main/java/com/axm/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git mst");
         System.out.println("hello git hf");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
